74fbf46ec58d70ebca2eadcd109c58b40d141d3d
Mike Perry <mike@vicino.io>
2017-06-17 15:36:12 -0400

    first example and repo structure

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # advanced-unix-programming
-Examples and exercises from Advanced Programming in the Unix Environment 
+Examples and exercises from Advanced Programming in the Unix Environment
+
+## Table of contents
+
+- [ ] [Chapter 1.   UNIX System Overview](Chapter-1/)
+- [ ] [Chapter 2.  UNIX Standardization and Implementations](Chapter-2/)
+- [ ] [Chapter 3.  File I/O](Chapter-3)
+- [ ] [Chapter 4.  Files and Directories](Chapter-4)
+- [ ] [Chapter 5.  Standard I/O Library](Chapter-5)
+- [ ] [Chapter 6.  System Data Files and Information](Chapter-6)
+- [ ] [Chapter 7.  Process Environment](Chapter-7)
+- [ ] [Chapter 8.  Process Control](Chapter-8)
+- [ ] [Chapter 9.  Process Relationships](Chapter-9)
+- [ ] [Chapter 10.  Signals](Chapter-10)
+- [ ] [Chapter 11.  Threads](Chapter-11)
+- [ ] [Chapter 12.  Thread Control](Chapter-12)
+- [ ] [Chapter 13.  Daemon Processes](Chatper-13)
+- [ ] [Chapter 14.  Advanced I/O](Chapter-14)
+- [ ] [Chapter 15.  Interprocess Communication](Chapter-15)
+- [ ] Chapter 16.  Network IPC: Sockets
+- [ ] Chapter 17.  Advanced IPC
+- [ ] Chapter 18.  Terminal I/O
+- [ ] Chapter 19.  Pseudo Terminals
+- [ ] Chapter 20.  A Database Library
+- [ ] Chapter 21.  Communicating with a Network Printer
---
+++ lib/error.c
@@ -0,0 +1,111 @@
+#include "apue.h"
+#include <errno.h>
+#include <stdarg.h>
+
+static void err_doit(int, int, const char *, va_list);
+
+/**
+ * Nonfatal error related to a system call
+ * Print a message and return
+ */
+
+void err_ret(const char *fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    err_doit(1, errno, fmt, ap);
+    va_end(ap);
+}
+
+/**
+ * Fatal err related to a system call.
+ * Print a message and terminate
+ */
+void err_sys(const char *fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    err_doit(1, errno, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
+/**
+ * Nonfatal error unrelated to a system call.
+ * Error code pass as explicit parameter.
+ * print a message and return
+ */
+void err_cont(int error, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(1, error, fmt, ap);
+    va_end(ap);
+}
+
+/**
+ * Fatal error unrelatd to a system call.
+ * Error code passed as explicit parameter.
+ * Print a message and terminate.
+ */
+void err_exit(int error, const char *fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    err_doit(1, error, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
+/**
+ * Fatal error related to a system call
+ * print a message, dump core, and terminate
+ */
+void err_dump(const char *fmt, ...) {
+    va_list ap;
+
+    va_start(ap, fmt);
+    err_doit(1, errno, fmt, ap);
+    va_end(ap);
+    abort();
+    exit(1);
+}
+
+/**
+ * Nonfatal error unrelated to a system call.
+ * print a message and return
+ */
+void err_msg(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(0, 0, fmt, ap);
+    va_end(ap);
+}
+
+/**
+ * Fatal Error unrelated to a system call
+ * print a message and terminate
+ */
+void err_quit(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    err_doit(0, 0, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
+/*
+ * Print a message and return to caller.
+ * Caller specifies "errnoflag"
+ */
+static void err_doit(int errnoflag, int error, const char *fmt, va_list ap) {
+    char buf[MAXLINE];
+
+    vsnprintf(buf, MAXLINE-1, fmt, ap);
+    if (errnoflag) {
+        snprintf(buf+strlen(buf), MAXLINE-strlen(buf)-1, ": %s", strerror(error));
+    }
+    strcat(buf, "\n");
+    fflush(stdout);
+    fputs(buf, stderr);
+    fflush(NULL); // flushes all stdio output streams
+}
